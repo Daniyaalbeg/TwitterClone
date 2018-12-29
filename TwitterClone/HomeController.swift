328db63a5f1eedecc1f6d108ffcbb49f57ba926b
Daniyaalbeg <danyaalbeg@gmail.com>
2018-12-29 18:57:21 +0000

    Added collection view and test cells

--- TwitterClone/HomeController.swift
+++ TwitterClone/HomeController.swift
@@ -8,9 +8,26 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController {
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    let cellID = "cellID"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .white
+        collectionView.register(CustomCell.self, forCellWithReuseIdentifier: "cellID")
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
     }
 }
